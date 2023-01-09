80770e9345052b5b82f2169c01158c3093afc00c
shoekzema <57985160+shoekzema@users.noreply.github.com>
2023-01-09 21:22:11 +0100

    Audio Options

--- scripts/Options.gd
+++ scripts/Options.gd
@@ -1,12 +1,30 @@
 extends Control
 
+onready var _masterbus := AudioServer.get_bus_index("Master")
+onready var _musicbus  := AudioServer.get_bus_index("Music")
+onready var _soundbus  := AudioServer.get_bus_index("SoundEffects")
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$VBoxContainer/Back.grab_focus()
+	$VBoxContainer/GlobalVolumeSlider.grab_focus()
+	$VBoxContainer/GlobalVolumeSlider.value = db2linear(AudioServer.get_bus_volume_db(_masterbus))
+	$VBoxContainer/MusicSlider.value = db2linear(AudioServer.get_bus_volume_db(_musicbus))
+	$VBoxContainer/SoundSlider.value = db2linear(AudioServer.get_bus_volume_db(_soundbus))
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
+# Back Button
 func _on_Back_pressed():
-	get_tree().change_scene("res://scenes/Main-Menu.tscn")
+	var _error = get_tree().change_scene("res://scenes/Main-Menu.tscn")
+
+
+# Volume Sliders
+func _on_GlobalVolumeSlider_value_changed(value: float):
+	AudioServer.set_bus_volume_db(_masterbus, linear2db(value))
+
+func _on_GlobalVolumeSlider_mouse_exited():
+	release_focus()
+
+func _on_MusicSlider_value_changed(value: float):
+	AudioServer.set_bus_volume_db(_musicbus, linear2db(value))
+
+func _on_SoundSlider_value_changed(value: float):
+	AudioServer.set_bus_volume_db(_soundbus, linear2db(value))
